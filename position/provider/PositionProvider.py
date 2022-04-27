05a2f05c76e97bca1c91b684c3d1ff45b304364a
Eugene the Red <me@eugenethered.com>
2022-04-27 14:54:02 +0100

    obtaining new position when slip is not used

--- position/provider/PositionProvider.py
+++ position/provider/PositionProvider.py
@@ -15,6 +15,12 @@ class PositionProvider:
 
     def obtain_position(self) -> Position:
         position_slip = self.position_slip_repository.retrieve()
+        if position_slip.status is Status.USED:
+            return self.position_repository.retrieve()
+        else:
+            return self.obtain_position_based_on_non_used_slip(position_slip)
+
+    def obtain_position_based_on_non_used_slip(self, position_slip):
         positions = self.position_supplier.get_positions()
         position = self.choose_position_based_on_slip(positions, position_slip)
         if position is not None:
@@ -22,13 +28,13 @@ class PositionProvider:
             self.position_repository.store(position)
         return position
 
-    def use_position_slip(self, position_slip):
-        position_slip.status = Status.USED
-        self.position_slip_repository.store(position_slip)
-
     @staticmethod
     def choose_position_based_on_slip(positions, position_slip) -> Position:
-        eligible_positions = [p for p in positions if p.instrument == position_slip.instrument and position_slip.status is not Status.USED]
+        eligible_positions = [p for p in positions if p.instrument == position_slip.instrument]
         if len(eligible_positions) > 0:
             return eligible_positions[0]
         return None
+
+    def use_position_slip(self, position_slip):
+        position_slip.status = Status.USED
+        self.position_slip_repository.store(position_slip)
