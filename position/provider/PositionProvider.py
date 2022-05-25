1518c5b8350a93fd2e0a167b090dc450b76c8828
Eugene the Red <me@eugenethered.com>
2022-05-25 11:10:38 +0100

    better position slip handling

--- position/provider/PositionProvider.py
+++ position/provider/PositionProvider.py
@@ -15,12 +15,10 @@ class PositionProvider:
         self.position_repository = position_repository
         self.position_slip_repository = position_slip_repository
 
-    def obtain_position(self) -> Position:
+    def updating_position(self):
         position_slip = self.position_slip_repository.retrieve()
-        if position_slip.status is Status.USED:
-            return self.position_repository.retrieve()
-        else:
-            return self.obtain_position_based_on_non_used_slip(position_slip)
+        if position_slip is not None and position_slip.status is not Status.USED:
+            self.obtain_position_based_on_non_used_slip(position_slip)
 
     def obtain_position_based_on_non_used_slip(self, position_slip):
         positions = self.position_supplier.get_positions()
@@ -28,13 +26,14 @@ class PositionProvider:
         if position is not None:
             self.use_position_slip(position_slip)
             self.position_repository.store(position)
-        return position
 
     @staticmethod
     def choose_position_based_on_slip(positions, position_slip) -> Optional[Position]:
         eligible_positions = [p for p in positions if p.instrument == position_slip.instrument]
         if len(eligible_positions) > 0:
-            return eligible_positions[0]
+            position = eligible_positions[0]
+            position.quantity = position_slip.quantity
+            return position
         return None
 
     def use_position_slip(self, position_slip):
